edc46d65cfad9c58da4b873ce0cb82337a30c982
Zach Eggert <egger0a6@email.arizona.edu>
2022-06-01 16:12:36 -0400

    add state vars, add some cached element refs, add init() and render() functions

--- js/app.js
+++ js/app.js
@@ -4,10 +4,13 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
+let turn, winner, board;
 
 
 /*------------------------ Cached Element References ------------------------*/
 
+const squareEls = document.querySelectorAll(".board-square");
+const messageEl = document.querySelector("#message");
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -16,3 +19,42 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
+init();
+
+function init() {
+  board = [ null, null, null,
+            null, null, null,
+            null, null, null];
+  turn = 1;
+  winner = null;
+
+  render();
+}
+
+
+function render() {
+  board.forEach(renderSquare);
+  if(winner === null) {
+    messageEl.textContent = `${(turn === 1) ? "X" : "O"}'s turn`;
+  }
+  else if (winner === "T") {
+    messageEl.textContent = "Tie Game"
+  }
+  else {
+    messageEl.textContent = `Congratulations! ${(turn === 1) ? "X" : "O"} won!`
+  }
+}
+
+// render() forEach callback function
+function renderSquare(sqr, indx) {
+  if (sqr === 1) {
+    console.log(squareEls[indx]);
+    squareEls[indx].style.backgroundColor = "blue";
+  }
+  else if (sqr === -1) {
+    squareEls[indx].style.backgroundColor = "red";
+  }
+  else {
+    // TODO style square based on null
+  }
+}
